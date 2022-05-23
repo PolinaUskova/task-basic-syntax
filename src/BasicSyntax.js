dfc6be0f516d3915d67ca910bdae3c47063c71f8
Polina <polina.uskova1535@gmail.com>
2022-05-23 14:15:39 +0500

    Задание по Syntax

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,25 @@
 export function romanToInteger(str) {
-    let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
-    return result;
+    let romanNotation = {
+        M: 1000,
+        D: 500,
+        C: 100,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1,
+    };
+    return str
+        .toUpperCase()
+        .split('')
+        .reduce(function (s, v, i, array) {
+            const [a, b, c] = [
+                romanNotation[array[i]],
+                romanNotation[array[i + 1]],
+                romanNotation[array[i + 2]],
+            ];
+            return b > a ? s - a : s + a;
+        }, 0);
 }
